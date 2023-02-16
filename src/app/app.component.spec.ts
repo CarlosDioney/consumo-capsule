3e8ddf9e174bc524977eabf9afa6a8a7e86746a4
CarlosDioney <a3521110116@alumno.uttehuacan.edu.mx>
2023-02-15 20:12:16 -0600

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'capsule'`, () => {
+  it(`should have as title 'Capsules-5A'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('capsule');
+    expect(app.title).toEqual('Capsules-5A');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('capsule app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Capsules-5A app is running!');
   });
 });
